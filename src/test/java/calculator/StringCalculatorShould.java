19035fe247ddf4652f19f029ee4487801449dd15
UMA CHAVAN <umachavan1994@gmail.com>
2021-10-17 12:07:15 +0530

    Test: negative number exception started

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -1,6 +1,5 @@
 package calculator;
 
-
 import static org.testng.Assert.assertEquals;
 
 import org.testng.annotations.Test;
@@ -8,38 +7,45 @@ import org.testng.annotations.Test;
 @Test
 class StringCalculatorShould {
 	StringCalculator stringCalculator = new StringCalculator();
-    @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
-    }
 
-   @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
-    }
-   
-   @Test
-   public void twoNumbers_Comma_Delimited_Returns_Sum() {
-		 assertEquals(3, stringCalculator.add("1,2"));
+	@Test
+	void empty_string_should_return_0() {
+		StringCalculator stringCalculator = new StringCalculator();
+		assertEquals(0, stringCalculator.add(""));
 	}
-   
- //Test 2 :handle an unknown amount of numbers
-   @Test
- 	public void amountofNumbers_CommaDelimited_ReturnsSum() {
- 		assertEquals(7, stringCalculator.add("1,2,1,3"));
- 	}
-   
- //Test 3:Allow the Add method to handle new lines between numbers
-   @Test
- 	public void Numbers_NewLine_Delimited_ReturnsSum() {	
-	   assertEquals(6, stringCalculator.add("1\n2,3"));
- 	}
-   
- //Test 4:Semi colon separator
-   @Test
- 	public void Numbers_semicol_Delimited_ReturnsSum() {	
-	   assertEquals(3, stringCalculator.add("1;2"));
- 	}
+
+	@Test
+	void string_with_single_number_should_return_number_as_int() {
+		StringCalculator stringCalculator = new StringCalculator();
+		assertEquals(1, stringCalculator.add("1"));
+	}
+
+	@Test
+	public void twoNumbers_Comma_Delimited_Returns_Sum() {
+		assertEquals(3, stringCalculator.add("1,2"));
+	}
+
+	// Test 2 :handle an unknown amount of numbers
+	@Test
+	public void amountofNumbers_CommaDelimited_ReturnsSum() {
+		assertEquals(7, stringCalculator.add("1,2,1,3"));
+	}
+
+	// Test 3:Allow the Add method to handle new lines between numbers
+	@Test
+	public void Numbers_NewLine_Delimited_ReturnsSum() {
+		assertEquals(6, stringCalculator.add("1\n2,3"));
+	}
+
+	// Test 4:Semi colon separator
+	@Test
+	public void Numbers_semicol_Delimited_ReturnsSum() {
+		assertEquals(3, stringCalculator.add("1;2"));
+	}
+
+	// Test 5
+	@Test(expectedExceptions = Exception.class)
+	public void negativeInputReturnsException() {
+		stringCalculator.add("-1");
+	}
 }
