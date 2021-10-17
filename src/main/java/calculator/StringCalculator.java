d993371c2d23a5cf497b8947cc520d1af7c94367
UMA CHAVAN <umachavan1994@gmail.com>
2021-10-17 11:07:21 +0530

    Test empty_string_should_return_0 passed

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,10 +2,8 @@ package calculator;
 
 class StringCalculator {
 
-
-
     public int add(String input) {
-        return 0;
+        return 0;   // Test empty_string_should_return_0: pass
     }
-
+    
 }
